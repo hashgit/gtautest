e64de83b74101b4a115e2a2ad269ec68684d0b0e
Imad <imad@Imads-MacBook-Pro.local>
2018-10-22 00:19:25 +1100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (<div>{
+
+<div>This is a test</div>
+
+}</div>);
+
+ReactDOM.render(<App />, document.getElementById('root'));
