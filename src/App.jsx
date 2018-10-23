3838ec8f522c3244bd1ff4c1feb0035d1dfd2247
hashgit <immaad@gmail.com>
2018-10-24 09:02:16 +1100

    added app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ContentBlock from './components/ContentBlock';
 
-const App = () => (<div>{
-
-<div>This is a test</div>
-
-}</div>);
+const App = () => (
+    <div>
+        {
+            <ContentBlock />
+        }
+    </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/components/ContentBlock.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/main.css';
+
+class ContentBlock extends React.Component {
+    render() {
+        return (
+            <div className="main">
+            Hello World
+            </div>
+        );
+    }
+}
+
+export default ContentBlock;
