5fe91a5fe78703ed5a0159b5cdd628b8b84f27e3
hashgit <immaad@gmail.com>
2018-10-25 08:09:45 +1100

    added router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ContentBlock from './components/ContentBlock';
 import './styles/main.css';
 
 const App = () => (
-    <div>
-        {
-            <ContentBlock />
-        }
-    </div>
+    <Router>
+        <Route exact path="/" component={ContentBlock} />
+    </Router>
 );
 
 ReactDOM.render(<App />, document.getElementById('root'));
